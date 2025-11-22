04f6d7aafafd34bb50845d4e918c8ef845e2e6a7
kimzuni <me@kimzuni.com>
2025-11-23 00:22:13 +0900

    port(rules): finish braces rule
    - add spacesOptions, spacesAfter, spacesBefore in rules/common.ts
    - update test:all script in package.json

---
+++ src/rules/braces.ts
@@ -0,0 +1,220 @@
+/*
+ * Copyright (C) 2016 Adrien Vergé
+ * Copyright (C) 2025 kimzuni
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ *
+ * Use this rule to control the use of flow mappings or number of spaces inside
+ * braces (``{`` and ``}``).
+ *
+ * .. rubric:: Options
+ *
+ * * ``forbid`` is used to forbid the use of flow mappings which are denoted by
+ *   surrounding braces (``{`` and ``}``). Use ``true`` to forbid the use of flow
+ *   mappings completely. Use ``non-empty`` to forbid the use of all flow
+ *   mappings except for empty ones.
+ * * ``min-spaces-inside`` defines the minimal number of spaces required inside
+ *   braces.
+ * * ``max-spaces-inside`` defines the maximal number of spaces allowed inside
+ *   braces.
+ * * ``min-spaces-inside-empty`` defines the minimal number of spaces required
+ *   inside empty braces.
+ * * ``max-spaces-inside-empty`` defines the maximal number of spaces allowed
+ *   inside empty braces.
+ *
+ * .. rubric:: Default values (when enabled)
+ *
+ * .. code-block:: yaml
+ *
+ *  rules:
+ *    braces:
+ *      forbid: false
+ *      min-spaces-inside: 0
+ *      max-spaces-inside: 0
+ *      min-spaces-inside-empty: -1
+ *      max-spaces-inside-empty: -1
+ *
+ * .. rubric:: Examples
+ *
+ * #. With ``braces: {forbid: true}``
+ *
+ *    the following code snippet would **PASS**:
+ *    ::
+ *
+ *     object:
+ *       key1: 4
+ *       key2: 8
+ *
+ *    the following code snippet would **FAIL**:
+ *    ::
+ *
+ *     object: { key1: 4, key2: 8 }
+ *
+ * #. With ``braces: {forbid: non-empty}``
+ *
+ *    the following code snippet would **PASS**:
+ *    ::
+ *
+ *     object: {}
+ *
+ *    the following code snippet would **FAIL**:
+ *    ::
+ *
+ *     object: { key1: 4, key2: 8 }
+ *
+ * #. With ``braces: {min-spaces-inside: 0, max-spaces-inside: 0}``
+ *
+ *    the following code snippet would **PASS**:
+ *    ::
+ *
+ *     object: {key1: 4, key2: 8}
+ *
+ *    the following code snippet would **FAIL**:
+ *    ::
+ *
+ *     object: { key1: 4, key2: 8 }
+ *
+ * #. With ``braces: {min-spaces-inside: 1, max-spaces-inside: 3}``
+ *
+ *    the following code snippet would **PASS**:
+ *    ::
+ *
+ *     object: { key1: 4, key2: 8 }
+ *
+ *    the following code snippet would **PASS**:
+ *    ::
+ *
+ *     object: { key1: 4, key2: 8   }
+ *
+ *    the following code snippet would **FAIL**:
+ *    ::
+ *
+ *     object: {    key1: 4, key2: 8   }
+ *
+ *    the following code snippet would **FAIL**:
+ *    ::
+ *
+ *     object: {key1: 4, key2: 8 }
+ *
+ * #. With ``braces: {min-spaces-inside-empty: 0, max-spaces-inside-empty: 0}``
+ *
+ *    the following code snippet would **PASS**:
+ *    ::
+ *
+ *     object: {}
+ *
+ *    the following code snippet would **FAIL**:
+ *    ::
+ *
+ *     object: { }
+ *
+ * #. With ``braces: {min-spaces-inside-empty: 1, max-spaces-inside-empty: -1}``
+ *
+ *    the following code snippet would **PASS**:
+ *    ::
+ *
+ *     object: {         }
+ *
+ *    the following code snippet would **FAIL**:
+ *    ::
+ *
+ *     object: {}
+ */
+
+import z from "zod";
+
+import { LintProblem } from "../linter";
+
+import type { TokenCheckProps } from "./types";
+import { spacesAfter, spacesBefore } from "./common";
+
+
+
+export const ID = "braces";
+export const TYPE = "token";
+export const CONF = z.object({
+	forbid: z.union([z.boolean(), z.literal("non-empty")]),
+	"min-spaces-inside": z.int(),
+	"max-spaces-inside": z.int(),
+	"min-spaces-inside-empty": z.int(),
+	"max-spaces-inside-empty": z.int(),
+});
+export const DEFAULT: Conf = {
+	forbid: false,
+	"min-spaces-inside": 0,
+	"max-spaces-inside": 0,
+	"min-spaces-inside-empty": -1,
+	"max-spaces-inside-empty": -1,
+};
+
+export type Conf = z.infer<typeof CONF>;
+
+
+
+export function* check({ conf, token }: TokenCheckProps<Conf>) {
+	if (
+		conf.forbid === true
+		&& token.data.type === "flow-map-start"
+	) {
+		yield new LintProblem(
+			token.startMark.line,
+			token.endMark.column,
+			"forbidden flow mapping",
+		);
+	} else if (
+		conf.forbid === "non-empty"
+		&& token.data.type === "flow-map-start"
+		&& token.next?.data.type !== "flow-map-end"
+	) {
+		yield new LintProblem(
+			token.startMark.line,
+			token.endMark.column,
+			"forbidden flow mapping",
+		);
+	} else if (
+		token.data.type === "flow-map-start"
+		&& token.next?.data.type === "flow-map-end"
+	) {
+		const problem = spacesAfter(token, {
+			min: conf["min-spaces-inside-empty"] !== -1 ? conf["min-spaces-inside-empty"] : conf["min-spaces-inside"],
+			max: conf["max-spaces-inside-empty"] !== -1 ? conf["max-spaces-inside-empty"] : conf["max-spaces-inside"],
+			minDesc: "too few spaces inside empty braces",
+			maxDesc: "too many spaces inside empty braces",
+		});
+		if (problem) yield problem;
+	} else if (token.data.type === "flow-map-start") {
+		const problem = spacesAfter(token, {
+			min: conf["min-spaces-inside"],
+			max: conf["max-spaces-inside"],
+			minDesc: "too few spaces inside braces",
+			maxDesc: "too many spaces inside braces",
+		});
+		if (problem) yield problem;
+	} else if (
+		token.data.type === "flow-map-end"
+		&& (token.prev?.data.type !== "flow-map-start")
+	) {
+		const problem = spacesBefore(token, {
+			min: conf["min-spaces-inside"],
+			max: conf["max-spaces-inside"],
+			minDesc: "too few spaces inside braces",
+			maxDesc: "too many spaces inside braces",
+		});
+		if (problem) yield problem;
+	}
+}
---
+++ tests/rules/braces.test.ts
@@ -0,0 +1,849 @@
+/*
+ * Copyright (C) 2016 Adrien Vergé
+ * Copyright (C) 2025 kimzuni
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+import { describe, test } from "node:test";
+
+import { ruleTestCase } from "../common";
+
+
+
+describe("Braces Test Case", () => {
+	const conf = ruleTestCase("braces");
+
+	test("disabled", async () => {
+		const check = await conf(
+			"braces: disable",
+		);
+
+		await check([
+			"---",
+			"dict1: {}",
+			"dict2: { }",
+			"dict3: {   a: 1, b}",
+			"dict4: {a: 1, b, c: 3 }",
+			"dict5: {a: 1, b, c: 3 }",
+			"dict6: {  a: 1, b, c: 3 }",
+			"dict7: {   a: 1, b, c: 3 }",
+			"",
+		], [
+		]);
+	});
+
+	describe("forbid", () => {
+		test("false", async () => {
+			const check = await conf(
+				"braces:",
+				"  forbid: false",
+			);
+
+			await check([
+				"---",
+				"dict: {}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {a}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {a: 1}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {",
+				"  a: 1",
+				"}",
+				"",
+			], [
+			]);
+		});
+
+		test("true", async () => {
+			const check = await conf(
+				"braces:",
+				"  forbid: true",
+			);
+
+			await check([
+				"---",
+				"dict:",
+				"  a: 1",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {}",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: {a}",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: {a: 1}",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: {",
+				"  a: 1",
+				"}",
+				"",
+			], [
+				[2, 8],
+			]);
+		});
+
+		test("non-empty", async () => {
+			const check = await conf(
+				"braces:",
+				"  forbid: non-empty",
+			);
+
+			await check([
+				"---",
+				"dict:",
+				"  a: 1",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {",
+				"}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {",
+				"# commented: value",
+				"# another: value2",
+				"}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {a}",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: {a: 1}",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: {",
+				"  a: 1",
+				"}",
+				"",
+			], [
+				[2, 8],
+			]);
+		});
+	});
+
+	describe("spaces", () => {
+		test("min", async () => {
+			let check: Awaited<ReturnType<typeof conf>>;
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: 0",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: {}",
+				"",
+			], [
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: 1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: {}",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: { }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {a: 1, b}",
+				"",
+			], [
+				[2, 8],
+				[2, 15],
+			]);
+
+			await check([
+				"---",
+				"dict: { a: 1, b }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {",
+				"  a: 1,",
+				"  b,",
+				"}",
+				"",
+			], [
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: 3",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: { a: 1, b }",
+				"",
+			], [
+				[2, 9],
+				[2, 17],
+			]);
+
+			await check([
+				"---",
+				"dict: {   a: 1, b   }",
+				"",
+			], [
+			]);
+		});
+
+		test("max", async () => {
+			let check: Awaited<ReturnType<typeof conf>>;
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: 0",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: {}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: { }",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: {a: 1, b}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: { a: 1, b }",
+				"",
+			], [
+				[2, 8],
+				[2, 16],
+			]);
+
+			await check([
+				"---",
+				"dict: {   a: 1, b   }",
+				"",
+			], [
+				[2, 10],
+				[2, 20],
+			]);
+
+			await check([
+				"---",
+				"dict: {",
+				"  a: 1,",
+				"  b,",
+				"}",
+				"",
+			], [
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: 3",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: {   a: 1, b   }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {    a: 1, b     }",
+				"",
+			], [
+				[2, 11],
+				[2, 23],
+			]);
+		});
+
+		test("both", async () => {
+			let check: Awaited<ReturnType<typeof conf>>;
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: 0",
+				"  max-spaces-inside: 0",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: {}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: { }",
+				"",
+			], [
+				[2, 8],
+			]);
+
+			await check([
+				"---",
+				"dict: {   a: 1, b}",
+				"",
+			], [
+				[2, 10],
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: 1",
+				"  max-spaces-inside: 1",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: {a: 1, b, c: 3 }",
+				"",
+			], [
+				[2, 8],
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: 0",
+				"  max-spaces-inside: 2",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"dict: {a: 1, b, c: 3 }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {  a: 1, b, c: 3 }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"dict: {   a: 1, b, c: 3 }",
+				"",
+			], [
+				[2, 10],
+			]);
+		});
+	});
+
+	describe("spaces empty", () => {
+		test("min", async () => {
+			let check: Awaited<ReturnType<typeof conf>>;
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: 0",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"array: {}",
+				"",
+			], [
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: 1",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"array: {}",
+				"",
+			], [
+				[2, 9],
+			]);
+
+			await check([
+				"---",
+				"array: { }",
+				"",
+			], [
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: 3",
+				"  max-spaces-inside-empty: -1",
+			);
+
+			await check([
+				"---",
+				"array: {}",
+				"",
+			], [
+				[2, 9],
+			]);
+
+			await check([
+				"---",
+				"array: {   }",
+				"",
+			], [
+			]);
+		});
+
+		test("max", async () => {
+			let check: Awaited<ReturnType<typeof conf>>;
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: 0",
+			);
+
+			await check([
+				"---",
+				"array: {}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"array: { }",
+				"",
+			], [
+				[2, 9],
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: 1",
+			);
+
+			await check([
+				"---",
+				"array: {}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"array: { }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"array: {  }",
+				"",
+			], [
+				[2, 10],
+			]);
+
+
+
+			check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: -1",
+				"  max-spaces-inside-empty: 3",
+			);
+
+			await check([
+				"---",
+				"array: {}",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"array: {   }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"array: {    }",
+				"",
+			], [
+				[2, 12],
+			]);
+		});
+
+		test("both", async () => {
+			const check = await conf(
+				"braces:",
+				"  min-spaces-inside: -1",
+				"  max-spaces-inside: -1",
+				"  min-spaces-inside-empty: 1",
+				"  max-spaces-inside-empty: 2",
+			);
+
+			await check([
+				"---",
+				"array: {}",
+				"",
+			], [
+				[2, 9],
+			]);
+
+			await check([
+				"---",
+				"array: { }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"array: {  }",
+				"",
+			], [
+			]);
+
+			await check([
+				"---",
+				"array: {   }",
+				"",
+			], [
+				[2, 11],
+			]);
+		});
+	});
+
+	test("mixed empty nonempty", async () => {
+		let check: Awaited<ReturnType<typeof conf>>;
+
+		check = await conf(
+			"braces:",
+			"  min-spaces-inside: 1",
+			"  max-spaces-inside: -1",
+			"  min-spaces-inside-empty: 0",
+			"  max-spaces-inside-empty: 0",
+		);
+
+		await check([
+			"---",
+			"array: { a: 1, b }",
+			"",
+		], [
+		]);
+
+		await check([
+			"---",
+			"array: {a: 1, b}",
+			"",
+		], [
+			[2, 9],
+			[2, 16],
+		]);
+
+		await check([
+			"---",
+			"array: {}",
+			"",
+		], [
+		]);
+
+		await check([
+			"---",
+			"array: { }",
+			"",
+		], [
+			[2, 9],
+		]);
+
+
+
+		check = await conf(
+			"braces:",
+			"  min-spaces-inside: -1",
+			"  max-spaces-inside: 0",
+			"  min-spaces-inside-empty: 1",
+			"  max-spaces-inside-empty: 1",
+		);
+
+		await check([
+			"---",
+			"array: { a: 1, b }",
+			"",
+		], [
+			[2, 9],
+			[2, 17],
+		]);
+
+		await check([
+			"---",
+			"array: {a: 1, b}",
+			"",
+		], [
+		]);
+
+		await check([
+			"---",
+			"array: {}",
+			"",
+		], [
+			[2, 9],
+		]);
+
+		await check([
+			"---",
+			"array: { }",
+			"",
+		], [
+		]);
+
+
+
+		check = await conf(
+			"braces:",
+			"  min-spaces-inside: 1",
+			"  max-spaces-inside: 2",
+			"  min-spaces-inside-empty: 1",
+			"  max-spaces-inside-empty: 1",
+		);
+
+		await check([
+			"---",
+			"array: { a: 1, b  }",
+			"",
+		], [
+		]);
+
+		await check([
+			"---",
+			"array: {a: 1, b   }",
+			"",
+		], [
+			[2, 9],
+			[2, 18],
+		]);
+
+		await check([
+			"---",
+			"array: {}",
+			"",
+		], [
+			[2, 9],
+		]);
+
+		await check([
+			"---",
+			"array: { }",
+			"",
+		], [
+		]);
+
+		await check([
+			"---",
+			"array: {   }",
+			"",
+		], [
+			[2, 11],
+		]);
+
+
+
+		check = await conf(
+			"braces:",
+			"  min-spaces-inside: 1",
+			"  max-spaces-inside: 1",
+			"  min-spaces-inside-empty: 1",
+			"  max-spaces-inside-empty: 1",
+		);
+
+		await check([
+			"---",
+			"array: { a: 1, b }",
+			"",
+		], [
+		]);
+
+		await check([
+			"---",
+			"array: {a: 1, b}",
+			"",
+		], [
+			[2, 9],
+			[2, 16],
+		]);
+
+		await check([
+			"---",
+			"array: {}",
+			"",
+		], [
+			[2, 9],
+		]);
+
+		await check([
+			"---",
+			"array: { }",
+			"",
+		], [
+		]);
+	});
+});
