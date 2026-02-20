8fc105240f6b682245dd00a1ec8a36159a9b575d
kimzuni <me@kimzuni.com>
2026-02-21 00:06:23 +0900

    build: dual cjs/esm exports

--- tsdown.config.js
+++ tsdown.config.js
@@ -4,23 +4,38 @@ import tsconfig from "./tsconfig.json" with { type: "json" };
 
 
 
+/** @type {import("tsdown").UserConfig} */
+
+const config = {
+	unbundle: true,
+	outDir: "./dist",
+	platform: "node",
+	target: tsconfig.compilerOptions.target,
+	hash: false,
+	dts: false,
+	minify: "dce-only",
+};
+
 export default defineConfig([
 	{
+		...config,
 		clean: true,
 		entry: [
-			"./src/main.ts",
 			"./src/index.ts",
 			"./src/internal.ts",
 		],
 		copy: [
 			"src/conf",
 		],
-		unbundle: true,
-		outDir: "./dist",
-		platform: "node",
-		target: tsconfig.compilerOptions.target,
-		hash: false,
-		dts: true,
-		minify: "dce-only",
+		format: "cjs",
+	},
+	{
+		...config,
+		entry: [
+			"./src/main.ts",
+			"./src/index.ts",
+			"./src/internal.ts",
+		],
+		format: "esm",
 	},
 ]);
